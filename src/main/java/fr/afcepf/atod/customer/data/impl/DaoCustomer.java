2273f2c96c4eb00572971b9ff513c0716bd357a5
ronan <ronan.lachater@gmail.com>
2017-03-28 16:36:01 +0200

    update daoCustomer

--- src/main/java/fr/afcepf/atod/customer/data/impl/DaoCustomer.java
+++ src/main/java/fr/afcepf/atod/customer/data/impl/DaoCustomer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.afcepf.atod.customer.data.impl;
 
 import fr.afcepf.atod.customer.data.api.IDaoCustomer;
